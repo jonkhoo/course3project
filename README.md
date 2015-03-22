7b9fd30fc19dd3f34fc52bccbfe37a7e381e8dda
Khoo Hui Tiong <khoohuit@gmail.com>
2015-03-22 18:53:39 +0800

    adding data.txt and run_analysis.R

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # course3project
 Getting and Cleaning Data Course Project
+
+Overview
+========
+1. Read the labels from features.txt.
+2. Select labels based on "std" or "mean" while ignoring the case. This gave 86 labels.
+3. Rename the labels to make it more meaningful.
+4. List the sub-directories under the current working directory. This assumes that each sub-directory contains the required data.
+5. For each of the sub directory, read the subject, activity and collected data.
+6. Join subject, activity and collected data into a table of 88 columns.
+7. Join the table from each sub-directory into a final table.
+8. Arrange the final table in order of subject and activity.
+9. Create a second data set by summarizing the value of each column grouping by subject and activity.
+10. Write the second data set into data.txt
+
+run_analysis.R
+==============
+This script can be ran in any directory containing the test and train data.
+It assumes that any of the sub directories found in the current directory
+to contain the necessary data.
+
+data.txt
+========
+This is the second data set created by run_analysis.R
+
+Codebook.md
+===========
+This contains the Codebook for the data created above by run_analysis.R
+
+
---
+++ run_analysis.R
@@ -0,0 +1,115 @@
+library(dplyr)
+
+# Function to read data from directory and returns a table
+# Inputs:
+#   dir:    (character) Name of directory containing data
+#   labels: (vector of characters) Headers for the columns
+#   headers:(vector of integers) Column numbers containing relevant data
+# Output:
+#   Table containing the relevant data
+# Assumptions:
+#   Given a directory named "xyz", there must exist the following files:
+#   xyz\X_xyz.txt:        data recorded from device
+#   xyz\y_xyz.txt:        activity that the data is based on
+#   xyz\subject_xyz.txt:  subject that the data is recorded from
+
+# Usage:
+# dir <- "test"
+# headers <- c(1:3)
+# labels <- c("a", "b", "c")
+# data <- read_data(dir, labels, headers)
+read_data <- function(dir, labels, headers) {
+  # Path to dir/X_dir.txt
+  file = paste0(dir, "/X_", dir, ".txt")
+  # Stop if file cannot be found
+  if (!file.exists(file)) {
+    msg <- paste(file, "not found!")
+    stop(msg)
+  }
+  # Read the data
+  x_data <- read.table(file)
+  x_tbl <- tbl_df(x_data)
+  x_tbl <- x_tbl %>%
+    select(headers) 
+  
+  # Path to dir/subject_dir.txt
+  file = paste0(dir, "/subject_", dir, ".txt")
+  # Stop if file cannot be found
+  if (!file.exists(file)) {
+    msg <- paste(file, "not found!")
+    stop(msg)
+  }
+  # Read the subject data
+  subject <- read.table(file)
+  
+  # Path to dir/y_dir.txt
+  file = paste0(dir, "/y_", dir, ".txt")
+  # Stop if file cannot be found
+  if (!file.exists(file)) {
+    msg <- paste(file, "not found!")
+    stop(msg)
+  }
+  # Read the activity data
+  activity <- read.table(file)
+  
+  # Combine the data sets
+  # col #1: subject 
+  # col #2: activity 
+  # col #3-88: data set
+  data <- cbind(subject, activity, x_tbl)
+  names(data) <- c("subject", "activity", labels)
+  data_tbl <- tbl_df(data)
+  
+  # Return the table 
+  return(data_tbl)
+}
+
+# Read the data for headers
+features <- read.table("features.txt")
+
+# Read the relevant data that has std or mean
+# We ignore the case here
+# Note: There is a difference in the number of columns based 
+# on whether we ignore the case. Without ignoring the case, 
+# we have 79 columns. By ignoring the case, we have 86 columns.
+headers <- grep("std|mean", features[,2], ignore.case=TRUE)
+
+# Rename the headers to make it more meaningful
+labels <- gsub("\\(\\)", "", features[headers, 2])
+labels <- gsub("\\(", "Of", labels)
+labels <- gsub("\\)", "", labels)
+labels <- gsub(",", "_", labels)
+
+
+# Read the activity label data
+# This is not used in this script
+# However, we can replace the numbers in activity with
+# the actual labels by using the following:
+#   activity_labels[activity[,1],2]
+# activity_labels <- read.table("activity_labels.txt")
+
+# Get the sub directories
+dirs <- list.dirs(".", recursive=FALSE)
+dirs <- gsub("\\./", "", dirs)
+
+# 1. Read the data from each of the sub directories into a table 
+# 2. Combine each table into a full table
+tmp_tbl <- data.frame()
+for (dir in dirs) {
+  tbl <- read_data(dir, labels, headers)
+  # Combine the data sets 
+  tmp_tbl <- rbind(tbl, tmp_tbl)
+}
+
+# First data set 
+# Sort the data by subject then activity
+data_tbl <- tmp_tbl %>%
+  arrange(subject, activity)
+
+# Second data set 
+# Each row contains the average
+avg_data_tbl <- data_tbl %>%
+  group_by(subject, activity) %>%
+  summarise_each(funs(mean))
+
+write.table(avg_data_tbl, file="data.txt", row.names=FALSE)
